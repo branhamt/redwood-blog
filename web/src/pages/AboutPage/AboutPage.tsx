4241aed0c6e679c2b009f4ae65482d06705ba923
Tyler Branham <tyler@trucksandbolts.com>
2023-02-17 12:12:12 -0500

    blog outline

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>This site was created to showcase Redwood JS!</p>
+    </>
+  )
+}
+
+export default AboutPage
